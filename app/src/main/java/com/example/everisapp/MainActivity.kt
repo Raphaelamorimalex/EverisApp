d3a69ad0757e3877a2dc12481a8735f7a5508b78
Raphaelamorimalex <67813638+Raphaelamorimalex@users.noreply.github.com>
2021-04-19 15:13:50 -0300

    Metodos activity adicionados

--- app/src/main/java/com/example/everisapp/MainActivity.kt
+++ app/src/main/java/com/example/everisapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.everisapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("LifeCycle", "Entrei no Start, deixei a tela visivel para você")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("LifeCycle","Entrei no Resume, pode interagir com a tela")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("LifeCycle","PAUSE, a tela perdeu o foco, você não pode mais interagir")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("LifeCycle", "Stop a tela não está visivel mas ainda existe")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("LifeCycle", "RESTART a tela está retornando o foco")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("LifeCycle", "Destroy- Tela foi destruida")
     }
 }
